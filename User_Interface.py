9a6f9d4efddb4151de8728866e98f6753889e143
Ryan Muriset <rmuriset@gmail.com>
2023-02-28 23:35:50 -0800

    Update User_Interface.py

--- User_Interface.py
+++ User_Interface.py
@@ -22,7 +22,14 @@ if inp == 'y':
                 print(item)
             ans1 = input("\nWould you like to roll for a random sweet to add to your shopping list? (y/n): ")
             if ans1 == 'y':
-                
+                f = open("treat.txt", 'w')
+                f.write(1)
+                f.close()
+            else:
+                ans = input("\nWould you like to view the current list? (y/n): ")
+                 if ans == 'y':
+                    for item in shopping_list:
+                        print(item)
             break
         shopping_list.append(new_item)
 
